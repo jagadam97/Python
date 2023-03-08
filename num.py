ab7d09063071fa80887e01f168d07dc489d30f0b
Dinesh Reddy J <dinesh.reddy.ext@juspay.in>
2023-03-08 23:22:17 +0530

    added numbers corey schafer -3 numbers

---
+++ num.py
@@ -0,0 +1,35 @@
+num = 3
+
+print(type(num))
+
+print(3 / 2) #keeps the decimal
+print(3 // 2) # drops the decimal
+print(3 * 2)
+print(3 ** 2) # Power of the numbers
+print(3 % 2) # modulus 
+print(4 % 2)
+print(3*2+1)
+print(3*(2+1))
+
+num = 1
+num = num + 1
+num *= 10
+print(num)
+
+print(abs(-3))
+print(round(3.75))
+print(round(3.75,1))
+
+#comparisons
+num1 = 3
+num2 = 2
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 >= num2)
+
+num_1 = '100'
+num_2 = '200'
+print(num_1+num_2)
